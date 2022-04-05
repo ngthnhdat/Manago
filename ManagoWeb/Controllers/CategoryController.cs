548c32ca0afc5cd05fcf85d5ba33a907e5ac7d56
Najin <ntd12112000@gmail.com>
2022-04-05 15:48:55 +0700

    Add project files.

---
+++ ManagoWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using ManagoWeb.Data;
+using ManagoWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagoWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View();
+        }
+    }
+}
---
+++ ManagoWeb/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ManagoWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManagoWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
