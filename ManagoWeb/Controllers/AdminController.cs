624d00723f8572df8dd0e647939cf36fd770e399
Najin <ntd12112000@gmail.com>
2022-04-07 13:25:22 +0700

    Added a lot of bullshit

---
+++ ManagoWeb/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using ManagoWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagoWeb.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly AppDbContext _db;
+        public AdminController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ManagoWeb/Controllers/CategoryController.cs
+++ ManagoWeb/Controllers/CategoryController.cs
@@ -14,7 +14,86 @@ namespace ManagoWeb.Controllers
         public IActionResult Index()
         {
             IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            /*if(obj.Name == obj.sthing.ToString())
+            {
+                ModelBinderAttribute.State.AddModelError("field", "custom error");
+            }*/
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) {
+                return NotFound();                    
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null) return NotFound();
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+                _db.Categories.Remove(categoryFromDb);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
         }
     }
 }
